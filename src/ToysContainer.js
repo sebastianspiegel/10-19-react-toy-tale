d228695bb71be1b91c8440207786b677fd54daa8
hansenjl <jenn.leigh.hansen@gmail.com>
2021-02-23 20:41:37 -0800

    starting point

---
+++ src/ToysContainer.js
@@ -0,0 +1,66 @@
+
+function ToysContainer(){
+    return(
+        <div id="toy-container">
+            <div>
+                <input type="text" placeholder="Search for a toy..." />
+            </div>
+            <div class="card" id="toy-1">
+                <h2>Woodt</h2>
+                <img src="http://www.pngmart.com/files/3/Toy-Story-Woody-PNG-Photos.png" class="toy-avatar"/>
+                <p>15 Likes </p>
+                <button class="like-btn">Like &lt;3</button>
+            </div>
+        
+            <div class="card" id="toy-2">
+                <h2>Buzz Lightyear</h2>
+                <img src="http://www.pngmart.com/files/6/Buzz-Lightyear-PNG-Transparent-Picture.png" class="toy-avatar"/>
+                <p>8 Likes </p>
+                <button class="like-btn">Like &lt;3</button>
+            </div>
+        
+            <div class="card" id="toy-3">
+                <h2>Mr. Potato Head</h2>
+                <img src="https://vignette.wikia.nocookie.net/universe-of-smash-bros-lawl/images/d/d8/Mr-potato-head-toy-story.gif/revision/latest?cb=20151129131217" class="toy-avatar"/>
+                <p>3 Likes </p>
+                <button class="like-btn">Like &lt;3</button>
+            </div>
+            <div class="card" id="toy-4">
+                <h2>Slinky Dog</h2>
+                <img src="https://www.freeiconspng.com/uploads/slinky-png-transparent-1.png" class="toy-avatar"/>
+                <p>4 Likes </p>
+                <button class="like-btn">Like &lt;3</button>
+            </div>
+            <div class="card" id="toy-5">
+                <h2>Rex</h2>
+                <img src="http://umich.edu/~chemh215/W11HTML/SSG5/ssg5.2/FRex.png" class="toy-avatar"/>
+                <p>3 Likes </p>
+                <button class="like-btn">Like &lt;3</button>
+            </div>
+    
+            <div class="card" id="toy-6">
+                <h2>Bo Peep</h2>
+                <img src="http://4.bp.blogspot.com/_OZHbJ8c71OM/Sog43CMFX2I/AAAAAAAADEs/0AKX0XslD4g/s400/bo.png" class="toy-avatar"/>
+                <p>2 Likes </p>
+                <button class="like-btn">Like &lt;3</button>
+            </div>
+   
+            <div class="card" id="toy-7">
+                <h2>Hamm</h2>
+                <img src="https://cdn140.picsart.com/244090226021212.png?r1024x1024" class="toy-avatar"/>
+                <p>0 Likes </p>
+                <button class="like-btn">Like &lt;3</button>
+            </div>
+     
+            <div class="card" id="toy-8">
+                <h2>Little Green Man</h2>
+                <img src="http://www.pngmart.com/files/3/Toy-Story-Alien-PNG-File.png" class="toy-avatar"/>
+                <p>-2 Likes </p>
+                <button class="like-btn">Like &lt;3</button>
+            </div>
+        </div>
+    )
+    
+}
+
+export default ToysContainer
